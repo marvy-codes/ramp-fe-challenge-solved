0b65909e391264bbf171093999a0df20af2d2768
marvyCodes <olowomarve@gmail.com>
2022-10-05 12:41:58 +0100

    fix: comparison

--- src/App.tsx
+++ src/App.tsx
@@ -64,7 +64,7 @@ export function App() {
             if (newValue === null) {
               return
             }
-            else if (newValue.id == "") {
+            else if (newValue.id === "") {
               await loadAllTransactions()
             }
             else await loadTransactionsByEmployee(newValue.id)
